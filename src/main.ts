fd95df56949d9eef2268a271538463d489a6b912
Johannes Voigt <j.voigt@finatix.de>
2020-04-26 13:16:04 +0200

    Add Swagger-UI

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { TweehtLogger } from 'logger/tweeht-logger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Setup App with logging
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log'],
   });
   app.useLogger(new TweehtLogger());
+
+  // Setup Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Tweeeht')
+    .setDescription('A simple twitterbot programmed with schlieferbebis in mind.')
+    .setVersion('1.0')
+    // .addTag('') //FIXME: use tags when some endpoints are ready
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
